e72b5fd662d22401ecf9d5df36c9ac4b5589f399
Shriram-Vibhute <shriram.vibhute08@gmail.com>
2024-02-10 16:10:51 +0530

    Python introduction and Print function indepth

---
+++ introduction.py
@@ -0,0 +1,12 @@
+print("hello world")
+a = 56
+print(a + 45)
+
+# print("Shriram" + 56); -> Only similar type variables will get added
+# b = "shrirm" + 13 -> This will also give an error
+
+# print functions parameters
+print("Hello Python", sep = " seperator ", end = "end")
+# The parameters al so contains -> file and flush
+print("Second Line")
+# default -> end = \n sep = " "
